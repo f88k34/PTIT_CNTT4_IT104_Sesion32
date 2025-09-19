a1d34b1423bf063eecb1d912c76f5d3df34d69b1
khanhh89 <khanhabcabc0000@gmail.com>
2025-09-19 09:20:24 +0700

    SS32

--- src/views/Couter.tsx
+++ src/views/Couter.tsx
@@ -2,20 +2,14 @@ import { useDispatch, useSelector } from "react-redux";
 import type { RootState, AppDispatch } from "../store/store";
 
 export default function Counter() {
-  // lấy state từ Redux (có type rõ ràng)
   const result = useSelector((state: RootState) => state.counts);
-
-  // dispatch có type rõ ràng
   const dispatch = useDispatch<AppDispatch>();
-
   const increase = () => {
     dispatch({ type: "INCREMENT" });
   };
-
   const reduce = () => {
-    dispatch({ type: "REDUCE" }); // bạn nên viết hoa cho đồng nhất
+    dispatch({ type: "REDUCE" })
   };
-
   return (
     <div>
       <h1>Ứng dụng Counter</h1>
