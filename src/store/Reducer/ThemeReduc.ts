a75a1ffe314bdd9d45faef73121c03d13351d26c
khanhh89 <khanhabcabc0000@gmail.com>
2025-09-19 09:18:55 +0700

    SS32

--- src/store/Reducer/ThemeReduc.ts
+++ src/store/Reducer/ThemeReduc.ts
@@ -1,14 +1,14 @@
-// src/store/reducers/themeReducer.ts
+
 const savedTheme = localStorage.getItem("darkMode") === "true";
 
 const initialState = {
-  darkMode: savedTheme, // mặc định lấy từ localStorage (nếu có)
+  darkMode: savedTheme,
 };
 
 export const themeReducer = (state = initialState, action: any) => {
   switch (action.type) {
     case "TOGGLE_THEME":
-      localStorage.setItem("darkMode", String(action.payload)); // lưu vào localStorage
+      localStorage.setItem("darkMode", String(action.payload));
       return { ...state, darkMode: action.payload };
     default:
       return state;
